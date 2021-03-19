88fde12e1dd7fd187f0634adc1d49c312ec97b87
Renbron <TranslucentZA@gmail.com>
2021-03-19 10:13:07 +0200

    Add project files.

---
+++ Git/Form1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Enter Label Name", "Label Changer", "", 100, 100);
+            label1.Text = input.ToString();
+        }
+    }
+}
